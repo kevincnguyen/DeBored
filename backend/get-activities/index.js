b653929187278ea13dd20f70551755f7c0bf95bf
Edwin Nino Delgado <enino@uw.edu>
2023-11-07 16:14:29 -0800

    added context

--- backend/get-activities/index.js
+++ backend/get-activities/index.js
@@ -2,7 +2,7 @@ import {MongoClient} from 'mongodb'
 
 const client = new MongoClient(process.env.MONGODB_URI)
 
-export const handler = async (event) => {
+export const handler = async (event, context) => {
     // This line allows us to return from the function before the connection to MongoDB Atlas is closed
     context.callbackWaitsForEmptyEventLoop = false;
     try{
