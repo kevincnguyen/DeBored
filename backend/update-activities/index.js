31dd4bc7a53c2110ec945f446371ff1c6375e2a7
kevincnguyen <kevincuunguyen@gmail.com>
2023-11-29 00:57:23 -0800

    Added update recent activities API

---
+++ backend/update-activities/index.js
@@ -0,0 +1,51 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export const handler = async (event, context) => {
+  // This line allows us to return from the function before the connection to MongoDB Atlas is closed
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    console.log(`Received event: ${JSON.stringify(event)}`);
+    const { id, activities } = JSON.parse(event.body);
+    console.log(`id: ${id}, activities: ${activities}`);
+
+    // Connect to MongoDB
+    await client.connect();
+    const usersCollection = client.db("DeBored").collection("Users");
+
+    // Retrieve the user
+    const user = await usersCollection.findOne({
+      _id: new ObjectId(id),
+    });
+    if (!user) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Invalid user ID received: ${id}`,
+        }),
+      };
+    }
+
+    // Append activities to user
+    await usersCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $push: { recentActivities: { $each: activities } } }
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Activities added successfully" }),
+    };
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Error with getting friends: ${error}`,
+      }),
+    };
+  }
+};
