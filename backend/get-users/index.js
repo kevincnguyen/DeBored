ddfbe35f97b3ec1152c83e68549d4511e5860798
kevincnguyen <kevincuunguyen@gmail.com>
2023-12-03 00:25:05 -0800

    Debugged GET all users

--- backend/get-users/index.js
+++ backend/get-users/index.js
@@ -1,36 +1,39 @@
-import { MongoClient, ObjectId } from "mongodb";
-import AWS from "aws-sdk";
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
 export const handler = async (event, context) => {
   // This line allows us to return from the function before the connection to MongoDB Atlas is closed
   context.callbackWaitsForEmptyEventLoop = false;
-	try{
-		console.log(`Received event: ${JSON.stringify(event)}`)
-		const {id} = JSON.parse(event.body)
-		await client.connect()
-		// Finds the current user
-		const result = await client.db("DeBored").collection('Users').findOne({ "_id": ObjectId(id) })
-		if (!result) {
-			throw new Error('Current user not found')
-		}
+  try {
+    console.log(`Received event: ${JSON.stringify(event)}`);
+    await client.connect();
 
-		// Gets all users besides the current user
-		const allUsers = await client.db("Debored").collection('Users').find(
-			{ "_id": { "$ne": ObjectId(id)}}).toArray()
-		if (!allUsers) {
-			throw new Error('No other users exist')
-		}
-		return {
-			statusCode: 200,
-			body: JSON.stringify({message: 'Potential friends found!'})
-		}
-	} catch(error) {
-		console.log(`Error: ${error}`)
-		return {
-			statusCode: 400,
-			body: JSON.stringify({message: `Error finding possible friends: ${error}`})
-		}
-	}
+    // Gets all users besides the current user
+    const allUsers = await client
+      .db("DeBored")
+      .collection("Users")
+      .find({}, { projection: { password: 0, friends: 0 } })
+      .toArray();
+
+    if (!allUsers) {
+      throw new Error("No other users exist");
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Potential friends found!",
+        allUsers: allUsers,
+      }),
+    };
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Error finding possible friends: ${error}`,
+      }),
+    };
+  }
 };
