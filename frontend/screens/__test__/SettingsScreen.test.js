ba2776ace1bb8926471ea85180e934d5bbe61f58
kevincnguyen <kevincuunguyen@gmail.com>
2023-12-04 13:40:04 -0800

    Fixed tests

--- frontend/screens/__test__/SettingsScreen.test.js
+++ frontend/screens/__test__/SettingsScreen.test.js
@@ -2,10 +2,16 @@ import React from "react";
 import renderer from "react-test-renderer";
 import SettingsScreen from "../SettingsScreen";
 import { useUser } from "../../contexts/UserContext";
+import { useImage } from "../../contexts/ImageContext";
 
 // Mock the user context
 jest.mock("../../contexts/UserContext", () => ({
   useUser: jest.fn(),
+}));
+
+// Mock the image context
+jest.mock("../../contexts/ImageContext", () => ({
+  useImage: jest.fn(),
 }));
 
 // Mock the react-native-paper components
@@ -21,11 +27,17 @@ test("renders SettingsScreen component correctly", () => {
   useUser.mockImplementation(() => ({
     user: { name: "", bio: "", email: "" },
   }));
+  useImage.mockImplementation(() => ({
+    updateImageKey: () => "",
+  }));
   const tree = renderer.create(<SettingsScreen />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test("contains all input fields with correct labels", () => {
+  useImage.mockImplementation(() => ({
+    updateImageKey: () => "",
+  }));
   const component = renderer.create(<SettingsScreen />);
   const textInputs = component.root.findAllByType("TextInput");
   // 9 inputs: name, bio, email, password, confirm password, phone, instagram, facebook, twitter
@@ -33,6 +45,9 @@ test("contains all input fields with correct labels", () => {
 });
 
 test("contains buttons for changing picture and saving changes", () => {
+  useImage.mockImplementation(() => ({
+    updateImageKey: () => "",
+  }));
   const component = renderer.create(<SettingsScreen />);
   const buttons = component.root.findAllByType("Button");
   expect(buttons.length).toBe(2); // 2 buttons: edit picture and save changes
