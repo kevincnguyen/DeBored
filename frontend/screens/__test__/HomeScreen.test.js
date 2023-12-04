2b113b739e25f72dc7691952c4d3e29690053dd0
kevincnguyen <kevincuunguyen@gmail.com>
2023-12-04 12:33:35 -0800

    Updated tests

--- frontend/screens/__test__/HomeScreen.test.js
+++ frontend/screens/__test__/HomeScreen.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { useUser } from "../../contexts/UserContext";
+import { useFriends } from "../../contexts/FriendsContext";
 
 import HomeScreen from "../HomeScreen";
 
@@ -9,9 +10,17 @@ jest.mock("../../contexts/UserContext", () => ({
   useUser: jest.fn(),
 }));
 
+// Mock the friends context
+jest.mock("../../contexts/FriendsContext", () => ({
+  useFriends: jest.fn(),
+}));
+
 test("renders HomeScreen component correctly", () => {
   useUser.mockImplementation(() => ({
     user: { location: "Seattle, WA" },
+  }));
+  useFriends.mockImplementation(() => ({
+    friends: [],
   }));
   const tree = renderer.create(<HomeScreen />).toJSON();
   expect(tree).toMatchSnapshot();
