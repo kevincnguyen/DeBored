26058c58bd1f5b616b7df3c342cc7e302e4e8415
kevincnguyen <kevincuunguyen@gmail.com>
2023-12-05 14:35:59 -0800

    Fixed tests

--- frontend/screens/__test__/QuizScreen.test.js
+++ frontend/screens/__test__/QuizScreen.test.js
@@ -1,11 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import QuizScreen from "../QuizScreen";
+import { useUser } from "../../contexts/UserContext";
 
 // Mock child components
 jest.mock("../../components/Quiz/StartQuiz", () => "StartQuiz");
 jest.mock("../../components/Quiz/QuizQuestion", () => "QuizQuestion");
 jest.mock("../../components/Quiz/QuizResults", () => "QuizResults");
+
+// Mock the user context
+jest.mock("../../contexts/UserContext", () => ({
+  useUser: jest.fn(),
+}));
 
 jest.mock("react-native-paper", () => ({
   Provider: "Provider",
@@ -16,6 +22,10 @@ jest.mock("react-native-paper-dropdown", () => ({
 }));
 
 test("renders StartQuiz component initially", () => {
+  useUser.mockImplementation(() => ({
+    user: { name: "", bio: "", email: "" },
+    updateUser: () => {},
+  }));
   const tree = renderer.create(<QuizScreen />).toJSON();
   expect(tree).toMatchSnapshot();
   expect(tree.children[0].children[0].type).toBe("StartQuiz");
